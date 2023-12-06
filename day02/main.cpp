ec6d34a35af2e901e20b705fbb95e39db7696b12
Jean Berniolles <jean@berniolles.fr>
2023-12-06 15:27:22 +0000

    day02: part one done

--- day02/main.cpp
+++ day02/main.cpp
@@ -23,6 +23,16 @@ std::vector<std::string> readLines(const std::string &filename)
     return lines;
 }
 
+std::string trim(const std::string &str)
+{
+    return str.substr(str.find_first_not_of(" "), str.find_last_not_of(" ") - str.find_first_not_of(" ") + 1);
+}
+
+bool endsWith(const std::string &str, const std::string &suffix)
+{
+    return str.size() >= suffix.size() && str.substr(str.size() - suffix.size()) == suffix;
+}
+
 std::string replaceAll(std::string str, const std::string &from, const std::string &to)
 {
     size_t start_pos = 0;
@@ -75,7 +85,7 @@ void replaceAll(std::string &str, const std::map<int, std::string> &map)
 
 void calculateFileValue(const std::string &filename)
 {
-    const std::map<int, std::string> digits =
+    const std::map<std::string, int> digits =
         {
             {"blue", 14},
             {"red", 12},
@@ -87,19 +97,73 @@ void calculateFileValue(const std::string &filename)
 
     for (auto const &line : lines)
     {
+        // For every game
         size_t id_sep = line.find(':');
 
         assert(id_sep != std::string::npos);
+        assert(line.substr(0, 5) == "Game ");
+
+        // std::cout << filename << " - " << line.substr(5, id_sep - 5) << std::endl;
+        const int id = std::stoi(line.substr(5, id_sep - 5));
 
         auto games = splitString(line.substr(id_sep + 1), ";");
 
         assert(games.size() != 0);
 
+        bool is_valid(true);
+
         for (const auto &game : games)
         {
+            // For every set
+            auto cubes(digits);
+
+            // Reset counters
+            for (auto &cube : cubes)
+            {
+                cubes[cube.first] = 0;
+            }
+
+            auto parts = splitString(game, ",");
+            assert(parts.size() <= digits.size());
+
+            for (const auto &part : parts)
+            {
+                // For every cube in a set
+                // std::cout << filename << " - " << trim(part) << std::endl;
+                auto parts = splitString(trim(part), " ");
+                assert(parts.size() == 2);
+
+                int part_value = std::stoi(trim(parts[0]));
+                assert(part_value != std::string::npos);
+
+                const std::string &part_color = parts[1];
+
+                assert(cubes.find(part_color) != cubes.end());
+
+                cubes[part_color] += part_value;
+            }
+
+            for (const auto &ref : digits)
+            {
+                assert(cubes.find(ref.first) != cubes.end());
+
+                if (ref.second < cubes[ref.first])
+                {
+                    is_valid = false;
+                    break;
+                }
+            }
+
+            if (!is_valid)
+            {
+                break;
+            }
         }
 
-        std::cout << "l: " << original << " m: " << line << " v: " << str_val << std::endl;
+        if (is_valid)
+        {
+            value += id;
+        }
     }
 
     std::cout << filename << " - " << count << " items - total: " << std::to_string(value) << std::endl;
@@ -107,11 +171,18 @@ void calculateFileValue(const std::string &filename)
 
 int main()
 {
-    const std::string directory = "input";
+    const std::string directory = "/workspaces/adventofcode-2023/day02/input";
 
     for (const auto &entry : std::filesystem::directory_iterator(directory))
     {
-        calculateFileValue(entry.path(), digits);
+        if (endsWith(entry.path(), ".txt"))
+        {
+            calculateFileValue(entry.path());
+        }
+        else
+        {
+            std::cout << "Skipping " << entry.path() << std::endl;
+        }
     }
 
     return 0;
